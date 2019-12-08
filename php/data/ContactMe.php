befc66dc6524682b720005eab1d76989f3332ff3
Kaur <p_kaur92032@TSS-F1000-MAC11.fc.ca>
2019-12-08 03:23:20 -0500

    added php folder

---
+++ php/data/ContactMe.php
@@ -0,0 +1,42 @@
+<?php
+    if(!empty($_POST)){
+        echo 'No...';
+        exit;
+    }
+
+    $name = '';
+    $email = '';
+    $subject = '';
+    $message = '';
+    $recipient = 'ilominati.test';
+
+    // some validations
+    if(isset($_POST['name'])){
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['email'])){
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '',$_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    if(isset($_POST['subject'])){
+        $subject = filter_var($_POST['subject'],FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['message'])){
+        $message = $_POST['message'];
+    }
+
+    $headers = [
+        'From'=>'noreply@test.ca',
+        'Reply-To'=>$name.'<'.$email.'>'
+    ];
+
+    if(mail($recipient, $subject, $message, $headers)){
+        echo '<p>Thanks for contacting,'.$name. '.you will get answer in next 24hrs.';
+    }else{
+        echo '<p>However we are sorry but the email was not transferred.</p>';
+    }
+
+?>
